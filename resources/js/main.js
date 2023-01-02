d293d938f8814770a285a24a7dbed31db488d295
Ashadul56 <mdashaduzzaman70149@gmail.com>
2023-01-03 00:24:59 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+/*WORK */
+var mixer = mixitup('.work-grid');
+// WOW 
+new WOW().init();
+// MENU 
+function openMenu() {
+    document.getElementById('menu-container').style.width ="100%";
+}
+function closeMenu() {
+    document.getElementById('menu-container').style.width ="0%";
+}
+
